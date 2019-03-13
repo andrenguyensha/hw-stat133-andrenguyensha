a88c261e79590a4c1752e31a275505ac1a8d960b
Andre Nguyen Sha <andre@calvisitor-10-105-182-26.calvisitor.1918.berkeley.edu>
2019-03-13 01:45:33 -0700

    almost done ahghah

---
+++ workout01/code/make-shots-data-script.R
@@ -0,0 +1,79 @@
+#Andre Sha's Data Preparation Phase Script
+#In this Data Script, I aggregate players' data and add columns
+#Inputs: andre-iguodala.csv, stephen-curry.csv, kevin-durant.csv, klay-thompson.csv, draymond-green.csv
+#Outputs: shots-data.csv, player data summaries
+
+data_types = c("team_name"="character", "game_date"="character", "season" = "integer", "period"="integer",
+               "minutes_remaining"="integer", "seconds_remaining"="integer", "shot_made_flag"="character",
+               "action_type"="factor", "shot_type"="factor", "shot_distance"="integer", "opponent"="character",
+               "x"="integer", "y"="integer")
+
+iguodala <- read.csv("../data/andre-iguodala.csv", stringsAsFactors = FALSE, colClasses = data_types)
+curry <- read.csv("../data/stephen-curry.csv", stringsAsFactors = FALSE, colClasses = data_types)
+durant <- read.csv("../data/kevin-durant.csv", stringsAsFactors = FALSE, colClasses = data_types)
+thompson <- read.csv("../data/klay-thompson.csv", stringsAsFactors = FALSE, colClasses = data_types)
+green <- read.csv("../data/draymond-green.csv", stringsAsFactors = FALSE, colClasses = data_types)
+
+#part 1.
+iguodala$name <- "Andre Iguodala"
+curry$name <- "Stephen Curry"
+durant$name <- "Kevin Durant"
+thompson$name <- "Klay Thompson"
+green$name <- "Draymond Green"
+
+#part 2. 
+iguodala$shot_made_flag[iguodala$shot_made_flag=="n"]<-"shot no"
+iguodala$shot_made_flag[iguodala$shot_made_flag=="y"]<-"shot yes"
+
+curry$shot_made_flag[curry$shot_made_flag=="n"]<-"shot no"
+curry$shot_made_flag[curry$shot_made_flag=="y"]<-"shot yes"
+
+durant$shot_made_flag[durant$shot_made_flag=="n"]<-"shot no"
+durant$shot_made_flag[durant$shot_made_flag=="y"]<-"shot yes"
+
+thompson$shot_made_flag[thompson$shot_made_flag=="n"]<-"shot no"
+thompson$shot_made_flag[thompson$shot_made_flag=="y"]<-"shot yes"
+
+green$shot_made_flag[green$shot_made_flag=="n"]<-"shot no"
+green$shot_made_flag[green$shot_made_flag=="y"]<-"shot yes"
+
+#part 3.
+iguodala$minute <- 12*iguodala$period - iguodala$minutes_remaining
+
+curry$minute <- 12*curry$period - curry$minutes_remaining
+
+durant$minute <- 12*durant$period - durant$minutes_remaining
+
+thompson$minute <- 12*thompson$period - thompson$minutes_remaining
+
+green$minute <- 12*green$period - green$minutes_remaining
+
+#part 4.
+sink(file = "../output/andre-iguodala-summary.txt")
+summary(iguodala)
+sink()
+
+sink(file = "../output/stephen-curry-summary.txt")
+summary(curry)
+sink()
+
+sink(file = "../output/kevin-durant-summary.txt")
+summary(durant)
+sink()
+
+sink(file = "../output/klay-thompson-summary.txt")
+summary(thompson)
+sink()
+
+sink(file = "../output/draymond-green-summary.txt")
+summary(green)
+sink()
+
+bindbindcrackers <- rbind(iguodala,curry,durant,thompson,green)
+
+write.csv(bindbindcrackers, file="../data/shots-data.csv")
+
+sink(file = "../output/shot-data-summary.txt")
+summary(bindbindcrackers)
+sink()
+
---
+++ workout01/data/data-dictionary.md
@@ -0,0 +1,13 @@
+team_name = This variable indicates the name of the team. 
+game_date = This variable indicates the date during which the game was played.
+season = This indicates the season in terms of year. 
+period = An NBA game is divided into 4 periods. so if period = 2, it refers to the second period (the second 12 minutes of the game). 
+minutes_remaining = Minutes remaining in the period that remained to be played.
+seconds_remaining = Seconds remaining in the period that remained to be played.
+shot_made_flag = Indicates whether or not the shot was made into the basket, either yes (y) or no (n). 
+action_type = The type of basketball manuever/move used by the player.
+shot_type = This indicates whether or not the shot is a 2-point FG or a 3-pointer.
+shot_distance = distance to the basket in feet.
+opponent = Opposing team name 
+x = Court coordinates in the horizontal axis, in inches, where the shot occured.
+y = Court coordinates in the vertical axis, in the inches, where the shot occurred. 
